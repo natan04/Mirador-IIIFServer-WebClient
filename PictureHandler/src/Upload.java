f6aa454f497d441b846665a632ba07ba9fa8fe69
Natan <natan04@gmail.com>
2015-06-06 00:41:31 +0300

    json impovment

--- PictureHandler/src/Upload.java
+++ PictureHandler/src/Upload.java
@@ -1,12 +1,15 @@
 
 
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
+import org.json.*;
 //import org.apache.commons.fileupload.FileItem;
 //import org.apache.commons.fileupload.FileUploadException;
 //import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -30,8 +33,8 @@ public class Upload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	  private boolean isMultipart;
 	   private String filePath;
-	   private int maxFileSize = 50 * 1024;
-	   private int maxMemSize = 4 * 1024;
+	   private int maxFileSize = 10	*1024 * 1024;
+	   private int maxMemSize =  10	* 1024 * 1024;
 	   private File file ;
  
     /**
@@ -48,8 +51,6 @@ public class Upload extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-
-
         response.setContentType("text/html");
         PrintWriter printWriter  = response.getWriter();
         printWriter.println("<h1>upload!</h1>");
@@ -122,10 +123,11 @@ public class Upload extends HttpServlet {
 	               file = new File( filePath + 
 	               fileName.substring(fileName.lastIndexOf("\\")+1)) ;
 	            }
-	            
-	            System.out.println(file.getPath());
+	            imageToAll(file, "IdOfBOOKS");
 	            fi.write( file ) ;
 	            out.println("Uploaded Filename: " + fileName + "<br>");
+	            
+	      
 	         }
 	      }
 	      out.println("</body>");
@@ -136,4 +138,58 @@ public class Upload extends HttpServlet {
 	
 
 }
+	
+	void imageToAll(File aFile,String idBook) throws JSONException
+	{
+		String name = "http://imagesrv/iipsrv/";
+		String port = "80";
+		String spec = "full/full/0/default.jpg";
+		String context = "http://iiif.io/api/image/2/context.json";
+		String idRes = name + idBook + "/" + aFile.getName() + "/full/full/0/default.jpg";
+		String idSer = name + idBook + "/" + aFile.getName();
+		String typeImage = "dctypes:Image";
+		String typeCanvas = "sc:Canvas";
+		String label = "Default label";
+		
+	      BufferedImage img = null;
+          try {
+              img = ImageIO.read(new File(aFile.getPath()));
+              System.out.println("height:" + img.getHeight() + " width:" + img.getWidth());
+              JSONObject toReturn = new JSONObject();
+
+              		  JSONObject firstImage = new JSONObject() ;
+		              firstImage.put("width", img.getWidth());
+		              firstImage.put("height", img.getHeight());
+		              
+		              	JSONObject resource = new JSONObject() ;
+		              	resource.put("@id", idRes);
+		              
+		              		JSONObject service = new JSONObject() ;
+		              		service.put("@context", context);
+		              		service.put("@id", idSer);
+		              	
+		              	resource.put("service", service);
+		              	
+		              firstImage.put("resource", resource);
+		              
+		              firstImage.put("@type", typeImage);
+		              firstImage.put("format", "image/jpeg");
+              
+	            toReturn.append("images", firstImage);
+   
+	            toReturn.put("width", img.getWidth());
+	            toReturn.put("height", img.getHeight());
+
+	            toReturn.put("@type", typeCanvas);
+	            toReturn.put("@type", label);
+              System.out.println(toReturn.toString());
+          } catch (IOException e) {
+          }
+	
+    
+          
+	
 	}
+	
+}
+	
